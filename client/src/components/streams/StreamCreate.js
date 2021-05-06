83694c2b071e5fcab285f6f3a8984ff39448cc21
Justin Wu <justinwu820@gmail.com>
2021-05-05 20:30:14 -0400

    create stream with redux-form

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+class StreamCreate extends React.Component {
+    renderError({ error, touched }) {
+        if (touched && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            )
+        }
+    }
+    renderInput = ({ input, label, meta }) => {
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} autoComplete="off" />
+                {this.renderError(meta)}
+            </div>
+        )
+    }
+    // we pass in this callback and formvalues will be store in an object.
+    onSubmit(formValues) {
 
-const StreamCreate = ()=>{
-    return <div>Stream Create</div>;
+    }
+    render() {
+        return (
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+                <Field name="title" component={this.renderInput} label="Enter Title" />
+                <Field name="description" component={this.renderInput} label="Enter Description" />
+                <button className="ui button primary">Submit</button>
+            </form>
+        )
+    }
 }
 
-export default StreamCreate;
+// Validation with Redux-Form
+const validate = (formValues) => {
+    // The error object key have to identical with the Field name, and the error message can be pass.
+    const error = {};
+    if (!formValues.title) {
+        error.title = "You must enter a title"
+    }
+    if (!formValues.description) {
+        error.description = "You must enter a description"
+    }
+    return error;
+};
+export default reduxForm({
+    form: 'streamCreate',
+    validate: validate
+})(StreamCreate);
